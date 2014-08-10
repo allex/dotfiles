f0d88ee2ccf0701d835a3ed4a8efdd57c184a7ce
Allex Wang <allex.wxn@gmail.com>
2014-08-10 12:40:43 +0800

    ci some utility toolkit

--- bin/killport.sh
+++ bin/killport.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 # ================================================
 # Description: killport.sh
 # GistID: 8b399a93749703acd780
 # GistURL: https://gist.github.com/allex/8b399a93749703acd780
-# Last Modified: Sun Jul 13, 2014 12:45PM
+# Last Modified: Sun Aug 10, 2014 12:22PM
 # Author: Allex Wang (allex.wxn@gmail.com)
 # ================================================
 
 port=$1
+ 
+if [[ -z "${port}" ]] && [[ -f /root/.bin/lsresin.sh ]]; then
+    . /root/.bin/lsresin.sh
+fi
 while [[ -z "${port}" ]]; do
     read -p "Please type the port you wanna kill (q to exit): " port
 done
-
 [ "${port}" = "q" ] && exit 0;
-
+ 
 PID=""
 case "`uname `" in
     Linux*)  PID=`lsof -t -i:${port}` ;;
@@ -23,7 +26,7 @@ esac
 
 if [ -n "$PID" ];
 then
-    kill -9 $PID && echo "procss with pid: ${PID} on port ${port} had killed success!"
+    kill -9 $PID && echo "process with pid: ${PID} on port ${port} had killed success!"
 else
     echo "The specific process with port '${port}' not found. exit";
 fi
